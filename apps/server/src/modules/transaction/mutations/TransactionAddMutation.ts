8bcab2c02de4b0f30359d4d1739e44c0a6b9cac6
Sun Stark <icaropereirasun@gmail.com>
2025-05-17 22:02:15 -0400

    feat(transaction): verify acconunts and balance to make a transaction

--- apps/server/src/modules/transaction/mutations/TransactionAddMutation.ts
+++ apps/server/src/modules/transaction/mutations/TransactionAddMutation.ts
@@ -1,6 +1,8 @@
-import { GraphQLInputObjectType, GraphQLNonNull, GraphQLString, GraphQLInt } from 'graphql';
+import { GraphQLInputObjectType, GraphQLNonNull, GraphQLError, GraphQLString, GraphQLInt } from 'graphql';
 import { Transaction } from '../transactionModel.ts';
 import { TransactionType } from '../transactionType.ts';
+
+import { Account } from '../../account/accountModel.ts';
 
 const SendTransactionInput = new GraphQLInputObjectType({
   name: 'SendTransactionInput',
@@ -18,6 +20,21 @@ export const TransactionAddMutation = {
       input: { type: new GraphQLNonNull(SendTransactionInput) },
     },
     resolve: async (_, { input }) => {
+      const fromAccount = await Account.findById(input.fromAccount);
+
+      if(!fromAccount || fromAccount.value - input.value < 0) {
+        return;
+      }
+
+      await Account.findOneAndUpdate(
+        { _id: input.toAccount }, 
+        { $inc: { balance: input.value }}); 
+
+      await Account.findOneAndUpdate(
+        {_id: fromAccount.id },
+        { $inc: { balance: - input.value }}
+      );
+
       const transaction = await new Transaction({
         fromAccount: input.fromAccount,
         toAccount: input.toAccount,
